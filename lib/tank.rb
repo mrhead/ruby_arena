8cc4b0665e8bc22a5d60e545f741c5f6c0527245
Patrik Bóna <patrik.bona@mrhead.sk>
2013-11-26 11:01:28 +0100

    Add possibility to move tank

--- lib/tank.rb
+++ lib/tank.rb
@@ -1,8 +1,13 @@
-class Tank
-  attr_reader :speed
+require 'gosu'
 
-  def initialize
+class Tank
+  attr_reader :x, :y, :speed, :heading
+
+  def initialize(args = {})
+    @x = args[:x] || 0
+    @y = args[:y] || 0
     @speed = 0
+    @heading = args[:heading] || 0
   end
 
   def accelerate
@@ -11,5 +16,20 @@ class Tank
 
   def decelerate
     @speed -= 1
+  end
+
+  def move
+    @x += offset_x(heading, speed)
+    @y += offset_y(heading, speed)
+  end
+
+  private
+
+  def offset_x(heading, speed)
+    Gosu.offset_x(heading, speed)
+  end
+
+  def offset_y(heading, speed)
+    Gosu.offset_y(heading, speed)
   end
 end
--- spec/tank_spec.rb
+++ spec/tank_spec.rb
@@ -3,7 +3,7 @@ require 'tank'
 describe Tank do
   describe '#accelerate' do
     it 'increases speed by 1' do
-      pp tank
+      tank.accelerate
 
       expect(tank.speed).to be 1
     end
@@ -14,6 +14,44 @@ describe Tank do
       tank.decelerate
 
       expect(tank.speed).to be(-1)
+    end
+  end
+
+  describe '#move' do
+    it 'changes y coordinate by 1 when speed is 1 and heading is 0' do
+      tank = Tank.new(y: 0, heading: 0)
+
+      tank.accelerate
+      tank.move
+
+      expect(tank.y).to eq(-1)
+    end
+
+    it 'changes y coordinate by -1 when speed is 1 and heading is 180' do
+      tank = Tank.new(y: 0, heading: 180)
+
+      tank.accelerate
+      tank.move
+
+      expect(tank.y).to eq(1)
+    end
+
+    it 'changes x coordinate by 1 when speed is 1 and heading is 90' do
+      tank = Tank.new(x: 0, heading: 90)
+
+      tank.accelerate
+      tank.move
+
+      expect(tank.x).to eq(1)
+    end
+
+    it 'changes x coordinate by -1 when speed is 1 and heading is 270' do
+      tank = Tank.new(x: 0, heading: 270)
+
+      tank.accelerate
+      tank.move
+
+      expect(tank.x).to eq(-1)
     end
   end
 
