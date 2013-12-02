3b4537da9837583827f22d4f4ce2dfa1b9f62636
Patrik Bóna <patrik.bona@mrhead.sk>
2013-12-02 11:54:02 +0100

    Render radar and rotate tank body

--- lib/robot_renderer.rb
+++ lib/robot_renderer.rb
@@ -9,15 +9,22 @@ class RobotRenderer
   end
 
   def draw
-    window.draw_quad(
-      x1, y1, color,
-      x2, y1, color,
-      x2, y2, color,
-      x1, y2, color
-    )
+    draw_tank_body
+    draw_radar
   end
 
   private
+
+  def draw_tank_body
+    window.rotate(heading, x, y) do
+      window.draw_quad(
+        x1, y1, color,
+        x2, y1, color,
+        x2, y2, color,
+        x1, y2, color
+      )
+    end
+  end
 
   def x
     robot.x
@@ -47,7 +54,49 @@ class RobotRenderer
     robot.size
   end
 
+  def heading
+    robot.heading
+  end
+
   def color
     Gosu::Color::WHITE
+  end
+
+  def draw_radar
+    color = Gosu::Color.new(40, 0, 255, 0)
+
+    window.draw_triangle(
+      x, y, color,
+      radar_x1, radar_y1, color,
+      radar_x2, radar_y2, color
+    )
+  end
+
+  def radar_x1
+    x + Gosu.offset_x(radar_heading - radar_view_angle / 2, radar_range)
+  end
+
+  def radar_x2
+    x + Gosu.offset_x(radar_heading + radar_view_angle / 2, radar_range)
+  end
+
+  def radar_y1
+    y + Gosu.offset_y(radar_heading - radar_view_angle / 2, radar_range)
+  end
+
+  def radar_y2
+    y + Gosu.offset_y(radar_heading + radar_view_angle / 2, radar_range)
+  end
+
+  def radar_heading
+    robot.radar_heading
+  end
+
+  def radar_view_angle
+    20
+  end
+
+  def radar_range
+    400
   end
 end
