ea3d9527069b869fbb8c18fcef22f75b0eb43737
Patrik Bóna <patrik.bona@mrhead.sk>
2013-12-06 11:18:12 +0100

    Add default speed to bullet

--- lib/bullet.rb
+++ lib/bullet.rb
@@ -3,12 +3,14 @@ require_relative 'movable'
 class Bullet
   include Movable
 
+  DEFAULT_SPEED = 3
+
   attr_reader :x, :y, :heading, :speed
 
   def initialize(args)
     @x = args.fetch(:x)
     @y = args.fetch(:y)
     @heading = args.fetch(:heading)
-    @speed = args.fetch(:speed)
+    @speed = args.fetch(:speed, DEFAULT_SPEED)
   end
 end
