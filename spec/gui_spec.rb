3eb7c766ad6a9f16f613fe6e49a88ae0abeebeb5
Abinoam Jr <abinoam@gmail.com>
2014-01-03 17:24:32 -0300

    Use let instead of instance variables

--- spec/gui_spec.rb
+++ spec/gui_spec.rb
@@ -1,34 +1,30 @@
 require 'fake_gui'
-require 'arena'
-require 'ai'
-require 'robot'
 
 describe Gui do
-  before(:each) do
-    @robot  = double('robot',
-                       heading: 0,
-                       radar_heading: 0,
-                       gun_heading: 0,
-                       x: 0,
-                       y: 0,
-                       size: 10,
-                       radar_view_angle: 10,
-                       radar_range: 100
-                      )
-    @bullet = double('bullet', heading: 0, x: 0, y: 0, size: 2)
-    @arena  = double('arena', width: 800, height: 600, robots: [@robot], bullets: [@bullet])
-    @gui    = FakeGui.new(@arena)
+  let(:robot) do
+    double('robot',
+            heading: 0,
+            radar_heading: 0,
+            gun_heading: 0,
+            x: 0,
+            y: 0,
+            size: 10,
+            radar_view_angle: 10,
+            radar_range: 100 )
   end
+  let(:bullet) { double('bullet', heading: 0, x: 0, y: 0, size: 2) }
+  let(:arena)  { double('arena', width: 800, height: 600, robots: [robot], bullets: [bullet]) }
+  let(:gui)    { FakeGui.new(arena) }
 
   describe 'public interface' do
-    it { expect(@gui).to respond_to(:draw) }
+    it { expect(gui).to respond_to(:draw) }
   end
 
   describe '#update' do
     it 'notifies arena' do
-      expect(@arena).to receive(:update)
+      expect(arena).to receive(:update)
 
-      @gui.update
+      gui.update
     end
   end
 
@@ -39,7 +35,7 @@ describe Gui do
 
       expect(robot_renderer).to receive(:draw)
 
-      @gui.draw
+      gui.draw
     end
 
     it 'notifies each bullet renderer' do
@@ -48,7 +44,7 @@ describe Gui do
 
       expect(bullet_renderer).to receive(:draw)
 
-      @gui.draw
+      gui.draw
     end
   end
 end
